548e4a435825b01b2087296a6fd9b709ef913d55
jakub bala <63164657+JakubBala@users.noreply.github.com>
2023-02-11 00:07:18 +0000

    [Feature]: Prefabbed InputHandler

--- Assets/Scripts/Interactables/Cannon.cs
+++ Assets/Scripts/Interactables/Cannon.cs
@@ -10,9 +10,12 @@ public class Cannon : InteractableBase
     public float lowerAngle = -45f;
     public float rotationSpeed = 10f;
 
+    bool rotatingToUpperAngle = true;
+
     private void Start()
     {
         interactionCycle = 0;
+        rotatingToUpperAngle = true;
     }
     /// <summary>
     /// First interact is angle, second interact is power
@@ -41,39 +44,35 @@ public class Cannon : InteractableBase
 
     private IEnumerator AngleSetStartAnimating_C()
     {
-        //
-
         while (true)
         {
-            bool rotatingToUpperAngle = true;
-
             print(transform.rotation.z * Mathf.Rad2Deg);
             if (rotatingToUpperAngle)
             {
                 print("rotating UP");
-                Quaternion rot = transform.rotation;
-                rot.z += rotationSpeed * Mathf.Deg2Rad * Time.deltaTime;
-                print(rot.z * Mathf.Rad2Deg);
+                Vector3 rot = transform.rotation.eulerAngles;
+                rot.z += rotationSpeed * Time.deltaTime;
+                print(rot.z);
 
-                if (rot.z * Mathf.Rad2Deg >= upperAngle)
+                if (rot.z >= upperAngle)
                 {
                     rotatingToUpperAngle = false;
                 }
-                transform.rotation = rot;
+                transform.rotation.SetEulerAngles(rot);
 
             }
 
             if (!rotatingToUpperAngle)
             {
                 print("rotating DOWN");
-                Quaternion rot = transform.rotation;
-                rot.z += rotationSpeed * Mathf.Deg2Rad * Time.deltaTime * -1f;
+                Vector3 rot = transform.rotation.eulerAngles;
+                rot.z += rotationSpeed * Time.deltaTime - 1f; 
 
-                if (rot.z * Mathf.Rad2Deg <= lowerAngle)
+                if (rot.z <= lowerAngle)
                 {
                     rotatingToUpperAngle = true;
                 }
-                transform.rotation = rot;
+                transform.rotation.SetEulerAngles(rot);
             }
 
             yield return null;
